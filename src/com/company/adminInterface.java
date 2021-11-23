2fa7c86c4fd30557003e14bad924d1322871179a
Sashko <xaniboyx@gmail.com>
2021-11-23 22:04:47 +0200

    addInterface to admin class

---
+++ src/com/company/adminInterface.java
@@ -0,0 +1,9 @@
+package com.company;
+
+import java.io.IOException;
+
+public interface adminInterface {
+     void readProtocols()throws IOException;
+     void addNewEmployee() throws IOException;
+     void addNewCustomer() throws IOException;
+}
