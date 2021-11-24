08a9eae8d020714f82d1392b94d271490c17f376
Viktor848 <viktority848@gmail.com>
2021-11-24 22:04:28 +0200

    Try and catch modification

--- src/com/company/Employee.java
+++ src/com/company/Employee.java
@@ -7,15 +7,20 @@ import java.util.Scanner;
 
 public class Employee  implements employeeInterface{
     public void protocol() throws IOException {
-        FileWriter fileWriter = new FileWriter("Employee's protocols.txt",true);
+        FileWriter fileWriter = new FileWriter("Employee's protocols.txt", true);
         PrintWriter out = new PrintWriter(fileWriter);
         Scanner input = new Scanner(System.in);
         System.out.println("За кои клиенти си работил/а днес?");
-        String customerName = input.nextLine();
-        System.out.println("Колко време отдели на клиента?");
-        String timeSpend = input.nextLine();
-        out.println(customerName);
-        out.println(timeSpend);
-        out.close();
+        try {
+            String customerName = input.nextLine();
+            System.out.println("Колко време отдели на клиента?");
+            String timeSpend = input.nextLine();
+            out.println(customerName);
+            out.println(timeSpend);
+            out.close();
+        }
+        catch (Exception e){
+            System.out.println("Грешни входни данни!");
+        }
     }
 }
