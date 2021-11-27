aa469da6be8a35497ba2cedf457c292d90c9ea79
Viktor848 <viktority848@gmail.com>
2021-11-27 22:26:11 +0200

    Try and catch modification

---
+++ src/com/company/Customer.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Customer {
+    public String name;
+    public String projectName;
+    public String dateOfEndOfContract;
+
+    public Customer(String name, String projectName,String dateOfEndOfContract){
+        this.name = name;
+        this.projectName = projectName;
+        this.dateOfEndOfContract = dateOfEndOfContract;
+    }
+}
